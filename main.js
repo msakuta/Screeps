1f56a537b7e82c0cc8e731839c5751a0819bc84a
msakuta <masahiro.sakuta@gmail.com>
2016-07-31 15:29:24 -0400

    Support arbitrary towers

--- main.js
+++ main.js
@@ -44,18 +44,22 @@ module.exports.loop = function () {
         stats.stats()
     }
 
-    var tower = Game.getObjectById('80dfba55e78128eeb35102df');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
+    for(var name in Game.rooms){
+        var room = Game.rooms[name]
+        var towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}})
+        for(var j = 0; j < towers.length; j++) {
+            var tower = towers[j]
+            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+            if(closestDamagedStructure) {
+                tower.repair(closestDamagedStructure);
+            }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
+            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if(closestHostile) {
+                tower.attack(closestHostile);
+            }
         }
     }
 

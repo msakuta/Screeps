9a037e636943f5933ab98abfbbbec3eca8d3cb5c
msakuta <masahiro.sakuta@gmail.com>
2016-08-02 02:22:45 -0400

    Rangers stay at flags with least sentry creeps

--- role.ranger.js
+++ role.ranger.js
@@ -7,8 +7,34 @@ module.exports = {
             if(ERR_NOT_IN_RANGE === creep.rangedAttack(enemy))
                 creep.moveTo(enemy)
         }
+        else if(!creep.memory.flag){
+            var flagNames = ['ranger', 'ranger2']
+            var flagWatchers = []
+
+            for(let i = 0; i < flagNames.length; i++){
+                flagWatchers[i] = {name: flagNames[i], count: 0}
+            }
+
+            // Game.creeps is game provided hash, so we don't want to iterate it
+            // too many times, hence outer loop.
+            for(let cname in Game.creeps){
+                c = Game.creeps[cname]
+                for(let i = 0; i < flagNames.length; i++){
+                    let flag = Game.flags[flagNames[i]]
+                    if(!flag)
+                        continue
+                    if(c.memory.flag === flagNames[i])
+                        flagWatchers[i].count++
+                }
+            }
+
+            flagWatchers.sort((a,b) => a.count - b.count)
+            var flag = Game.flags[flagWatchers[0].name]
+            if(flag)
+                creep.memory.flag = flag.name
+        }
         else{
-            var flag = Game.flags['ranger']
+            var flag = Game.flags[creep.memory.flag]
             if(flag && !flag.pos.isNearTo(creep.pos))
                 creep.moveTo(flag)
             else if(!creep.memory.ready){

d7f9261f6c93c946d05d6978b6dfea4bcfc637dd
msakuta <masahiro.sakuta@gmail.com>
2016-08-01 21:45:12 -0400

    Delegate builder's logic to harvester altogether
    Since harvester has more sophisticated precedence.

--- role.builder.js
+++ role.builder.js
@@ -1,4 +1,5 @@
 var roleUpgrader = require('role.upgrader')
+var roleHarvester = require('role.harvester')
 
 function builderFilter(x){
     var structHits = x.hitsMax
@@ -20,22 +21,22 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-	    if(creep.memory.building && creep.carry.energy == 0) {
+        if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.memory.target = undefined
             creep.say('harvesting');
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity){
+        }
+        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity){
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-	        var targets2 = creep.room.find(FIND_STRUCTURES, {filter: builderFilter});
+            var targets2 = creep.room.find(FIND_STRUCTURES, {filter: builderFilter});
             if(targets.length || targets2.length) {
                 creep.memory.building = true;
                 creep.memory.resting = false;
                 creep.say('building');
             }
-	    }
+        }
 
-	    if(creep.memory.building) {
+        if(creep.memory.building) {
             var target
             if(creep.memory.target && (target = Game.getObjectById(creep.memory.target))){
                 //console.log('builder target: '+ target.id + ', ' + (target instanceof Structure))
@@ -56,20 +57,22 @@ var roleBuilder = {
                     creep.memory.target = undefined;
             }
             else{
-    	        var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+                var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
                 if(target && false){
                     creep.memory.target = target.id
                 }
                 else{
-        	        var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: builderFilter});
+                    var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: builderFilter});
                     if(target){
                         creep.memory.target = target.id
                     }
                     else{
-            	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
                         if(targets.length) {
                             creep.memory.target = targets[0].id
                         }
+                        else
+                            creep.memory.target = undefined
                     }
                 }
                 if(!creep.memory.target){
@@ -77,45 +80,13 @@ var roleBuilder = {
                     creep.memory.target = undefined
                 }
             }
-	    }
-	    else if(creep.carry.energy < creep.carryCapacity){
-            var source = creep.pos.findClosestByRange(FIND_SOURCES, {filter: s => 0 < s.energy});
-            if(source && creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
-	    }
-        else{
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER)
-                            && structure.energy < structure.energyCapacity ||
-                            structure.structureType === STRUCTURE_CONTAINER && structure.store.energy < structure.storeCapacity;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-                creep.memory.resting = undefined;
-            }
-            else if(!creep.memory.resting){
-                creep.say('resting')
-                var flag = Game.flags['rest']
-                if(flag){
-                    creep.moveTo(flag)
-                    creep.memory.resting = true
-                }
-            }
-            else{
-                roleUpgrader.run(creep)
-                var flag = Game.flags['rest']
-                if(flag)
-                    creep.moveTo(flag)
-            }
         }
-	}
+        else{
+            // Delegate logic to harvester altogether since harvester has more
+            // sophisticated precedence.
+            roleHarvester.run(creep)
+        }
+    }
 };
 
 module.exports = roleBuilder;

189cc6623eb052333997a16220a637c40bc8040c
msakuta <masahiro.sakuta@gmail.com>
2016-08-05 11:05:34 -0400

    Update stats.js to include attackers

--- stats.js
+++ stats.js
@@ -22,13 +22,15 @@ module.exports = {
         for(var i in Game.rooms){
             var closestToExpire = 1500
             var room = Game.rooms[i]
-            var totalCreeps = [0,0,0,0]
-            var dyingCreeps = [0,0,0,0]
+            var totalCreeps = [0,0,0,0,0]
+            var dyingCreeps = [0,0,0,0,0]
             var restingCreeps = 0
             var creeps = room.find(FIND_MY_CREEPS)
             for(var j = 0; j < creeps.length; j++){
                 var creep = creeps[j]
-                var role = creep.memory.role === 'harvester' ? 0 : creep.memory.role === 'builder' ? 1 : creep.memory.role === 'ranger' ? 2 : 3
+                var role = ['harvester', 'builder', 'ranger', 'attacker', 'upgrader'].indexOf(creep.memory.role)
+                if(role < 0)
+                    continue
                 totalCreeps[role]++
                 if(creeps[j].ticksToLive < 200)
                     dyingCreeps[role]++
@@ -38,8 +40,8 @@ module.exports = {
                     closestToExpire = creep.ticksToLive
             }
             var creepStats = ''
-            for(var j = 0; j < 4; j++){
-                creepStats += dyingCreeps[j] + '/' + totalCreeps[j] + (j !== 3 ? ',' : '')
+            for(var j = 0; j < totalCreeps.length; j++){
+                creepStats += dyingCreeps[j] + '/' + totalCreeps[j] + (j !== totalCreeps.length-1 ? ',' : '')
             }
 
             var targets = Game.rooms[i].find(FIND_STRUCTURES, {filter: (x) => x.hits < x.hitsMax && x.hits < 3000});

38cbb9fa2d21ca8020b0207df01da4b2d3351f52
msakuta <masahiro.sakuta@gmail.com>
2016-08-11 11:43:17 -0400

    Make claimers reserve intelligently

--- dist/main.js
+++ dist/main.js
@@ -226,7 +226,8 @@ module.exports.loop = function () {
     if(claimers.length < maxClaimers) {
         tryCreateCreepInt('claimer', 0, [
             [CLAIM,CLAIM,CLAIM,CLAIM,CLAIM,MOVE,MOVE],
-            [CLAIM,MOVE,MOVE],
+            [CLAIM,CLAIM,MOVE,MOVE],
+            [CLAIM,MOVE],
         ])
     }
 
--- dist/role.claimer.js
+++ dist/role.claimer.js
@@ -6,14 +6,23 @@ var roleClaimer = {
 
         var roomName = Game.flags.reserve && Game.flags.reserve.room ? Game.flags.reserve.room.name : 'E48S13'
 
+        var roomCount = _.filter(Game.rooms, r => r.controller && r.controller.my).length
+
         if(creep.room.name !== roomName)
             creep.moveTo(new RoomPosition(25,25,roomName))
         else{
             var target = creep.room.controller
             if(target && !target.my){
                 if(target.level === 0){
-                    if(ERR_NOT_IN_RANGE === creep.claimController(target)){
-                        creep.moveTo(target)
+                    if(Game.gcl.level === roomCount){
+                        if(ERR_NOT_IN_RANGE === creep.reserveController(target)){
+                            creep.moveTo(target)
+                        }
+                    }
+                    else{
+                        if(ERR_NOT_IN_RANGE === creep.claimController(target)){
+                            creep.moveTo(target)
+                        }
                     }
                 }
             }

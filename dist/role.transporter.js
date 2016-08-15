90260557d3543f177049ba7be57311264c3a6374
msakuta <masahiro.sakuta@gmail.com>
2016-08-14 21:07:34 -0400

    Introduce cost-based task selection system to transporters
    This is essentially copy of harvester.
    
    Also made transporters be persistent with their goal room, because we
    don't want to see them going back and forth by alternating amount of
    energy each room has.

--- dist/role.transporter.js
+++ dist/role.transporter.js
@@ -34,51 +34,96 @@ module.exports = {
         }
         else if(creep.memory.task === 'gather'){
             if(creep.carry.energy === creep.carryCapacity){
+                creep.memory.toSpawn = undefined
                 creep.memory.task = 'store'
                 return
             }
             var fromSpawn = null
+            var freeCapacity = creep.carryCapacity - _.sum(creep.carry)
             //if(false && fromSpawn)
             {
-                let roomGathering = false
+                let taskIssued = false
                 if(fromSpawn && creep.room === fromSpawn.room || !creep.room.controller || !creep.room.controller.my){
-                    let resource = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {filter: r => 100 < r.amount})
-                    let path = resource ? creep.pos.findPathTo(resource) : null
-                    // Go to dropped resource if a valid path is found to it and worth it
-                    if(resource && path && path.length && walkCost(creep, path.length) < resource.amount){
-                        if(creep.pickup(resource) === ERR_NOT_IN_RANGE)
-                            creep.move(path[0].direction)
-                        roomGathering = true
+
+                    var tasks = []
+
+                    if(_.sum(creep.carry) < creep.carryCapacity){
+                        // Always try to find and collect dropped resources
+                        let target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {filter: r => 100 < r.amount});
+                        let path = target ? creep.pos.findPathTo(target) : null
+                        // Go to dropped resource if a valid path is found to it and worth it
+                        if(target && path && path.length && walkCost(creep, path.length) < target.amount){
+                            tasks.push({
+                                name: 'DropResource',
+                                // Dropped resources has slightly less cost, meaning high priority, than storages.
+                                cost: (creep.pos.getRangeTo(target) - .5) / Math.min(freeCapacity, target.amount),
+                                target: target,
+                                run: (target) => {
+                                    if(ERR_NOT_IN_RANGE === creep.pickup(target))
+                                        creep.moveTo(target)
+                                }
+                            })
+                        }
+
+                        let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: s => (s.structureType === STRUCTURE_CONTAINER ||
+                                s.structureType === STRUCTURE_STORAGE) &&
+                                0 < s.store.energy && s.room === creep.room
+                        })
+                        // Pick up dropped resources and withdraw from adjacent container
+                        // simultaneously, but precede resource by not issueing withdraw
+                        // order if resource amount is more than available space.
+                        if(container && creep.pos.getRangeTo(container) <= 1){
+                            tasks.push({
+                                name: 'Container',
+                                cost: creep.pos.getRangeTo(container) / Math.min(freeCapacity, container.store.energy),
+                                target: container,
+                                run: container => {
+                                    if(creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE)
+                                        creep.moveTo(container)
+                                }
+                            })
+                        }
                     }
-                    let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                        filter: s => (s.structureType === STRUCTURE_CONTAINER ||
-                            s.structureType === STRUCTURE_STORAGE) &&
-                            0 < s.store.energy && s.room === creep.room
-                    })
-                    // Pick up dropped resources and withdraw from adjacent container
-                    // simultaneously, but precede resource by not issueing withdraw
-                    // order if resource amount is more than available space.
-                    if(container && (!resource || creep.carryCapacity - _.sum(creep.carry) < resource.amount && creep.pos.getRangeTo(container) <= 1)){
-                        if(creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE && (!resource || creep.carryCapacity - _.sum(creep.carry) < resource.amount))
-                            creep.moveTo(container)
-                        roomGathering = true
-                    }
-                    else{
-                        let source = creep.pos.findClosestByRange(FIND_SOURCES, {filter: s => s.room === creep.room})
-                        if(source){
-                            if(creep.pos.getRangeTo(source) <= 2){
-                                let awayPath = PathFinder.search(creep.pos, {pos: source.pos, range: 3}, {flee: true}).path
-                                //console.log(awayPath)
-                                if(awayPath.length)
-                                    creep.moveTo(awayPath[awayPath.length-1])
-                            }
+
+                    let source = creep.pos.findClosestByRange(FIND_SOURCES, {filter: s => s.room === creep.room})
+                    if(source){
+                        if(creep.pos.getRangeTo(source) <= 2){
+                            tasks.push({
+                                name: 'AvoidSource',
+                                // There is no cost in avoidance, it's just be very low so that
+                                // avoidance won't happen when there are other important tasks.
+                                cost: creep.pos.getRangeTo(source),
+                                target: source,
+                                run: target => {
+                                    let awayPath = PathFinder.search(creep.pos, {pos: target.pos, range: 3}, {flee: true}).path
+                                    //console.log(awayPath)
+                                    if(awayPath.length)
+                                        creep.moveTo(awayPath[awayPath.length-1])
+                                }
+                            })
 //                            if(creep.harvest(source) === ERR_NOT_IN_RANGE){
 //                                creep.moveTo(source)
 //                            }
                         }
                     }
+
+                    // Task selector
+                    //if(!tasks.length)
+                    //    console.log(creep.name + ': tasks: ' + tasks.length)
+                    if(tasks.length){
+                        let bestTask = _.reduce(tasks, (best, task) => {
+                            if(task.cost < best.cost)
+                                return task
+                            return best
+                        })
+                        let costs = _.reduce(tasks, (str, task) => str += '[' + task.name +': ' + task.cost + '],', '')
+                        //console.log(creep.name + ': tasks: ' + tasks.length + ': bestTask: ' + bestTask.name + ', ' + bestTask.cost + ', ' + costs + ', ' + bestTask.target)
+                        bestTask.run(bestTask.target)
+                        taskIssued = true
+                    }
                 }
-                if(!roomGathering){
+                if(!taskIssued){
                     if(fromSpawn)
                         creep.moveTo(fromSpawn)
                     else if(Game.flags.dig)
@@ -99,15 +144,19 @@ module.exports = {
             if(creep.carry.energy === 0){
                 creep.memory.task = 'gather'
             }
-            var toSpawn = _.reduce(Game.spawns, (best, spawn) => {
-                var storage = spawn.room.storage
-                if(!storage)
-                    return best
-                var bestStorage = best.room.storage
-                if(!bestStorage)
-                    return spawn
-                return spawn.room.storage.store.energy < best.room.storage.store.energy ? spawn : best
-            })
+            var toSpawn = Game.getObjectById(creep.memory.toSpawn)
+            if(!toSpawn){
+                toSpawn = _.reduce(Game.spawns, (best, spawn) => {
+                    var storage = spawn.room.storage
+                    if(!storage)
+                        return best
+                    var bestStorage = best.room.storage
+                    if(!bestStorage)
+                        return spawn
+                    return spawn.room.storage.store.energy < best.room.storage.store.energy ? spawn : best
+                })
+                creep.memory.toSpawn = toSpawn.id
+            }
             if(toSpawn){
                 if(creep.room === toSpawn.room){
                     let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -132,7 +181,9 @@ module.exports = {
                 }
             }
         }
-        else
+        else{
             creep.memory.task = 'gather'
+            creep.memory.toSpawn = undefined
+        }
     }
 };
